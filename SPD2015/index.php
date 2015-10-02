0a94eb38b1de971dd28c39e4dd636db3afafcc8d
Steffen <steffen.voss@online.de>
2015-10-02 13:35:53 +0200

    Erste Schritte zu einem responsiven Theme, basierend auf Bootstrap 4

---
+++ SPD2015/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>SPD 2015</title>
+    <link rel="stylesheet" type="text/css" href="lib/bootstrap/dist/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  </head>
+
+  <body>
+    <div class="container" style="background-color:lightgrey;">
+      <div class="row">
+        <div class="col-md-4 col-md-offset-8">
+          <h1>AK Digitale Gesellschaft</h1>
+        </div>
+      </div>
+    </div>
+  </body>
+</html> 
