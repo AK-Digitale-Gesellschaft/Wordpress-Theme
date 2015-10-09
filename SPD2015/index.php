850d395b3cd1c66a9ab7eeee06a89325d41b7ab5
kaffeeringe <kontakt@kaffeeringe.de>
2015-10-09 22:57:47 +0200

    Navbar hinzugefügt. JavaScript löppt.

--- SPD2015/index.php
+++ SPD2015/index.php
@@ -10,6 +10,7 @@
 
   <body>
     <div class="container">
+		
       <div class="row visible-lg-block" id="lg-header">
 				<div id="logo">
 					<a href="#" title="Zurück zur Startseite"><img src="img/SPD-Logo.jpg" width="100" height="100" alt="Logo der SPD" /></a>
@@ -19,6 +20,34 @@
 					<p id="slogan">Sozial. Gerecht. Vor Ort.</p>
         </div>
       </div>
+
+			<nav id="lg-navbar" class="navbar navbar-default visible-lg-block">
+				<div class="container-fluid">
+					<!-- Collect the nav links, forms, and other content for toggling -->
+					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+						<ul class="nav navbar-nav">
+						  <li><a id="homelink" href="#"><img src="img/home.png" alt="Logo der SPD" /></a></li>
+						  <li><a href="#">Link</a></li>
+						  <li class="dropdown">
+						    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+						    <ul class="dropdown-menu">
+						      <li><a href="#">Action</a></li>
+						      <li><a href="#">Another action</a></li>
+						      <li><a href="#">Something else here</a></li>
+						      <li role="separator" class="divider"></li>
+						      <li><a href="#">Separated link</a></li>
+						      <li role="separator" class="divider"></li>
+						      <li><a href="#">One more separated link</a></li>
+						    </ul>
+						  </li>
+						</ul>      
+					</div><!-- /.navbar-collapse -->
+				</div><!-- /.container-fluid -->
+			</nav>
+
     </div>
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+		<script src="lib/bootstrap/dist/js/bootstrap.min.js"></script>
   </body>
 </html> 
