56c75a46a4066694014fea2c991f8dfc8e570d8e
kaffeeringe <kontakt@kaffeeringe.de>
2015-10-10 17:02:53 +0200

    Basically the layout now works. Next step: Turn this template into a real Wordpress theme

--- SPD2015/index.php
+++ SPD2015/index.php
@@ -1,53 +1,21 @@
-<!DOCTYPE html>
-<html>
-	<head>
-    <meta charset="UTF-8">
-    <title>SPD 2015</title>
-    <link rel="stylesheet" type="text/css" href="lib/bootstrap/dist/css/bootstrap.css">
-    <link rel="stylesheet" type="text/css" href="style.css">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-  </head>
+<?php
+/**
+* I try to figure out if user uses a computer, a tablet or a phone and redirect her to the right template
+**/
+require_once 'lib/mobile-detect/Mobile_Detect.php';
+$detect = new Mobile_Detect;
 
-  <body>
-    <div class="container">
-		
-      <div class="row visible-lg-block" id="lg-header">
-				<div id="logo">
-					<a href="#" title="Zurück zur Startseite"><img src="img/SPD-Logo.jpg" width="100" height="100" alt="Logo der SPD" /></a>
-				</div>
-        <div id="title" class="col-lg-offset-7 col-lg-5">
-          <h1>Arbeitskreis Digitale Gesellschaft</h1>
-					<p id="slogan">Sozial. Gerecht. Vor Ort.</p>
-        </div>
-      </div>
+$deviceType = ($detect->isMobile() ? ($detect->isTablet() ? 'tablet' : 'phone') : 'computer');
+$scriptVersion = $detect->getScriptVersion();
 
-			<nav id="lg-navbar" class="navbar navbar-default visible-lg-block">
-				<div class="container-fluid">
-					<!-- Collect the nav links, forms, and other content for toggling -->
-					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-						<ul class="nav navbar-nav">
-						  <li><a id="homelink" href="#"><img src="img/home.png" alt="Logo der SPD" /></a></li>
-						  <li><a href="#">Link</a></li>
-						  <li class="dropdown">
-						    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-						    <ul class="dropdown-menu">
-						      <li><a href="#">Action</a></li>
-						      <li><a href="#">Another action</a></li>
-						      <li><a href="#">Something else here</a></li>
-						      <li role="separator" class="divider"></li>
-						      <li><a href="#">Separated link</a></li>
-						      <li role="separator" class="divider"></li>
-						      <li><a href="#">One more separated link</a></li>
-						    </ul>
-						  </li>
-						</ul>      
-					</div><!-- /.navbar-collapse -->
-				</div><!-- /.container-fluid -->
-			</nav>
-
-    </div>
-		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-		<script src="lib/bootstrap/dist/js/bootstrap.min.js"></script>
-  </body>
-</html> 
+switch ($deviceType) {
+    case 'computer':
+        include('lg-index.php');
+        break;
+    case 'tablet':
+        include('md-index.php');
+        break;
+    case 'phone':
+        include('xs-index.php');
+        break;
+}
---
+++ SPD2015/lg-index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+	<head>
+    <meta charset="UTF-8">
+    <title>SPD 2015</title>
+    <link rel="stylesheet" type="text/css" href="lib/bootstrap/dist/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  </head>
+
+  <body>
+    <div class="container">
+		
+      <div class="row visible-lg-block" id="lg-header">
+				<div id="logo">
+					<a href="#" title="Zurück zur Startseite"><img src="img/SPD-Logo.jpg" width="100" height="100" alt="Logo der SPD" /></a>
+				</div>
+        <div id="title" class="col-lg-offset-7 col-lg-5">
+          <h1>Arbeitskreis Digitale Gesellschaft</h1>
+					<p id="slogan">Sozial. Gerecht. Vor Ort.</p>
+        </div>
+      </div>
+
+			<nav id="lg-navbar" class="navbar navbar-default visible-lg-block">
+				<div class="container-fluid">
+					<!-- Collect the nav links, forms, and other content for toggling -->
+					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+						<ul class="nav navbar-nav">
+						  <li><a id="homelink" href="#"><img src="img/home.png" alt="Logo der SPD" /></a></li>
+						  <li><a href="#">Link</a></li>
+						  <li class="dropdown">
+						    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+						    <ul class="dropdown-menu">
+						      <li><a href="#">Action</a></li>
+						      <li><a href="#">Another action</a></li>
+						      <li><a href="#">Something else here</a></li>
+						      <li role="separator" class="divider"></li>
+						      <li><a href="#">Separated link</a></li>
+						      <li role="separator" class="divider"></li>
+						      <li><a href="#">One more separated link</a></li>
+						    </ul>
+						  </li>
+						</ul>      
+					</div><!-- /.navbar-collapse -->
+				</div><!-- /.container-fluid -->
+			</nav>
+
+			<div class="row visible-lg-block" id="lg-main-content">
+				<div class="col-lg-7" id="content">.col-md-7</div>
+				<aside class="col-lg-5" id="right-sidebar">.col-md-5</aside>
+			</div>
+
+			<footer class="row visible-lg-block" id="lg-footer">
+				<div class="col-lg-4" id="lg-footer-left">.col-md-4</div>
+				<div class="col-lg-4" id="lg-footer-center">.col-md-4</div>
+				<div class="col-lg-4" id="lg-footer-right">.col-md-4</div>
+			</footer>
+
+    </div>
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+		<script src="lib/bootstrap/dist/js/bootstrap.min.js"></script>
+  </body>
+</html> 
