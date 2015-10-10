77dbe37ae139e2e99d68f17994e34217614874da
kaffeeringe <kontakt@kaffeeringe.de>
2015-10-10 23:54:26 +0200

    Ähm ja, auf der Startseite wurde gar nicht gecheckt, ob es ein Artikelbild gibt. Es wurde immer eingebaut…

--- SPD2015/loop.php
+++ SPD2015/loop.php
@@ -1,6 +1,8 @@
 <?php while (have_posts()) : the_post(); ?>
 	<div class="post">
-		<a href="<?php the_permalink() ?>" rel="bookmark" title="Link auf <?php the_title_attribute(); ?>" class="homeImg"><?php the_post_thumbnail('homepage-thumb');?></a>
+		<?php if ( has_post_thumbnail() ) { ?>
+			<a href="<?php the_permalink() ?>" rel="bookmark" title="Link auf <?php the_title_attribute(); ?>" class="homeImg"><?php the_post_thumbnail('homepage-thumb');?></a>
+		<?php  }	 ?>
 		<h2><small><?php the_category('/') ?><span class="hidden">: </span></small><br>		
 		<a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a></h2>
 		<?php remove_more_jump_link(the_content('mehr…'), false); ?>
